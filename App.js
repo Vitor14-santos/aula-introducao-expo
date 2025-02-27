362592310d554a9c2a779fd9427d2dd6d5e2fd90
Vitor14-santos <vis860532@gmail.com>
2025-02-26 22:49:40 -0300

    ajustes layout

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Constants from 'expo-constants';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi vitoria!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
+    //alignItens: 'center
+    // '
     alignItems: 'center',
     justifyContent: 'center',
+    pedding: 8,
+    peddingTop: Constants.statusBarHeight || 8,
   },
 });
